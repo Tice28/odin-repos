7880650fe5937282f4430c8a7f918f71ebadc58f
trisston <trisstonprogramming@gmail.com>
2024-02-06 23:25:13 -0600

    Remove Duplicate Files From Previous Projects

--- README.md
+++
@@ -1,1 +0,0 @@
-This repository serves to contain all of my projects from The Odin Project. Each project will have its own branch. Feel free to poke around and clone to see what my projects look like. 
--- src/display.js
+++
@@ -1,7 +0,0 @@
-export default function removeContentChild(){
-    const content = document.getElementById("content");
-
-    if(content.hasChildNodes()){
-        const contentChild = content.removeChild(content.firstChild);
-    }
-}
