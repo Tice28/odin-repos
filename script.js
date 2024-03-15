f14b13ceb4a531747af9eede3aacad386ad1cc95
trisston <trisstonprogramming@gmail.com>
2024-03-14 20:27:59 -0500

    Add getTail, at, pop functions

--- script.js
+++ script.js
@@ -38,11 +38,56 @@ function createLinkedList(node = null){
         incrementSize();
     }
 
+    const getTail = () => {
+        let temp = head;
+        let next = temp.hasNext();
+        while(next){
+            temp = temp.getNext();
+            next = temp.hasNext();
+        }
+        return temp;
+    }
+
+    const at = (index) => {
+        if(index > getSize() - 1){
+            console.error("Bad index");
+            return null;
+        }
+        else{
+            let i = 0;
+            let temp = head;
+    
+            while(i < index){
+                temp = temp.getNext();
+                i = i+1;
+            }
+    
+            return temp;
+        }
+    }
+
+    const pop = () => {
+        let temp = head;
+        let val;
+        console.log(temp.getVal());
+        while(temp.getNext().hasNext()){
+            temp = temp.getNext();
+            console.log(temp.getVal());
+        }
+        
+        val = temp.getNext();
+        temp.setNext();
+        return val;
+    }
+
     return {
         getHead,
         getSize,
         prepend,
-        append
+        append,
+        getTail,
+        at,
+        pop
     }
 }
 
@@ -64,7 +109,7 @@ function createNode(nodeVal="null", nodeNext="null"){
         val = nodeVal;
     }
 
-    const setNext = (nodeNext) =>{
+    const setNext = (nodeNext="null") =>{
         next = nodeNext;
     }
 
@@ -87,6 +132,11 @@ function createNode(nodeVal="null", nodeNext="null"){
 }
 
 const node = createNode(0);
-console.log(node.hasNext())
 const node2 = createNode(8);
+const node3 = createNode(3);
+const node4 = createNode(5);
+//5 0 3 8
 const linkedList = createLinkedList(node);
+linkedList.append(node3);
+linkedList.append(node2);
+linkedList.prepend(node4);
